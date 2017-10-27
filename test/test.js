9bd1b16309440750e0a3ca76d6177fe6b0504dbb
JCloudYu <j.cloud.yu@gmail.com>
2017-10-27 16:58:19 +0800

    Add test script

---
+++ test/test.js
@@ -0,0 +1,18 @@
+(()=>{
+	"use strict";
+	
+	const pitaya = require( '../pitaya' ).chain({handler:(control, result)=>{
+		console.log( 0, result );
+		control.next = [
+			(control, result)=>{ console.log(1, result); return 'a'; },
+			(control, result)=>{ console.log(2, result); return 'b'; },
+			(control, result)=>{ return new Promise((fulfill)=>{
+				setTimeout(()=>{ console.log(3, result); fulfill('c'); }, 5000)
+			});},
+			(control, result)=>{ console.log(4, result); return 'd'; },
+			(control, result)=>{ console.log(5, result); return 'e'; },
+		];
+		
+		return 'k';
+	}}).trigger().then((result)=>{console.log(`final ${result}`)});
+})();
