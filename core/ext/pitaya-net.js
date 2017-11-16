0be85f3728d1a46a49e63341a497afcbc871ddd1
JCloudYu <j.cloud.yu@gmail.com>
2017-11-17 05:20:42 +0800

    Add full http path handling logics to HTTPParseQuery and HTTPPullPathComp apis

--- core/ext/pitaya-net.js
+++ core/ext/pitaya-net.js
@@ -26,9 +26,14 @@
 		StreamDrain:(stream)=>{
 			return exports.StreamReadAll(stream, true);
 		},
-		HTTPParseQuery:(queryStr)=>{
+		HTTPParseQuery:(queryStr, fullPath=true)=>{
 			queryStr = queryStr || '';
 			let query = {var:{}, flag:[]};
+			
+			if ( fullPath ) {
+				let q = queryStr.indexOf( '?' );
+				queryStr = (q < 0) ? queryStr : queryStr.substring(q+1);
+			}
 			queryStr.split( '&' ).forEach((item)=>{
 				let eq = item.indexOf('=');
 				if (eq < 0) {
@@ -41,14 +46,16 @@
 			
 			return query;
 		},
-		HTTPPullPathComp:(path)=>{
+		HTTPPullPathComp:(path, fullPath=true)=>{
 			if ( typeof path === "string" ) {
 				path = {url:path, comp:''};
 			}
-		
+			
 			let {comp, url} = path;
+			let limit = !fullPath ? url.length : url.indexOf( '?' );
+			
 			let divider = url.indexOf('/', 1);
-			if ( divider >= 0 ) {
+			if ( divider >= 0 && divider < limit ) {
 				comp = url.substring(0,divider);
 				url  = url.substring(divider);
 			}
